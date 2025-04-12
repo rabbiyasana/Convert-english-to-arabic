1aadaa2a94e2734aa0747803bf4c67131afa46fa
rabbiyasana <rabbiasana55@gmail.com>
2025-04-12 18:09:25 +0800

    update code with UI components

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -7,19 +7,51 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { Colors } from '@/constants/Colors';
+import { useEffect } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 export default function TabTwoScreen() {
+  let text= ''
+
+  useEffect(()=>{
+    console.log("useEffect===============>")
+    
+    // convertText('Indeed, Allah is best planner', 'arabic');
+  },[])
+
   return (
           <SafeAreaView style={{ flex: 1, }}>
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map((_, i) => {
-                       return <View style={{
+                       return <View key={i} style={{
                         padding:10,
-                        borderWidth:2
+                         flexDirection:'row',
+                        borderWidth:1,
+                        borderColor:'grey',
                        }}>
-                        <Text>{_}</Text>
+                        <View style={{flex:1, borderWidth:1, borderColor:'grey', padding:10, backgroundColor:'white'}}>
+                          <Text>{text}</Text>
+                        </View>
+                        <View style={{flexDirection:'row', gap:10, alignItems:'center'}}>
+                        <Ionicons
+                          name='play'
+                          size={18}
+                          color={Colors.light.icon}
+                        />
+                        <Ionicons
+                          name='trash'
+                          size={18}
+                          color={Colors.light.icon}
+                        />
+                        <Ionicons
+                          name='copy'
+                          size={18}
+                          color={Colors.light.icon}
+                        />
+                        </View>
                        </View>
                      })}
     </ParallaxScrollView>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,62 @@
-import { Image, StyleSheet,View, Platform, TextInput, ScrollView, Text, SafeAreaView, StatusBar } from 'react-native';
+import { Image, StyleSheet,View, Platform, TextInput, ScrollView, Text, SafeAreaView, StatusBar, Button, Pressable } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useState } from 'react';
+import { Colors } from '@/constants/Colors';
 
 export default function HomeScreen() {
+
+  const [text, setText] = useState('')
+  const [dua, setDua] = useState<any>([])
+
+  const convertText = async (text: string, language: string) => {
+      const genAI = new GoogleGenerativeAI('');
+              const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+              console.log("convertText",text)
+              const prompt = `convert this text ${text} to ${language} language in context of dua and prayers and add Ḍammah Kasrah Fatḥah to translated text and return single option and only arabic no other text in response`;
+              console.log("convertText",prompt)
+              const response = await model.generateContent(prompt);
+              console.log("response.response.text()",response.response.text())
+              setDua([...dua,{id :Math.floor(Math.random() * 10), text: response.response.text()}])
+              // text = response.response.text()
+              // return  response.response.text()
+      }
   return (
       <SafeAreaView style={{ flex: 1, }}>
-     <TextInput value={''} onChange={e=>{}} style={{ borderWidth:1, padding:5, margin:15}} />
+      <View style={{ flexDirection:'column',  padding:15, gap:10 }}>
+      <TextInput numberOfLines={2} value={text} onChangeText={setText} style={{ 
+        borderWidth:1,borderColor:`${Colors.light.border}`, padding:10}} />
+      <View style={{
+         borderRadius:20,
+         borderColor:'grey',
+         width:35,
+         borderWidth:1,
+         alignSelf:'flex-end',
+      }} >
+        <Pressable onPress={()=>convertText(text, 'arabic')}>
+        <MaterialCommunityIcons
+        style={{padding:5,}}
+            name='google-translate'
+            size={24}
+            color={Colors.light.icon}
+          />
+        </Pressable>
+        </View>
+      </View>
  <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}>
-               {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map((_, i) => {
+               {dua.map((_:any, i:number) => {
                  return <View style={{
                   padding:10,
                   borderWidth:2
                  }}>
-                  <Text>{_}</Text>
+                  <Text>{_.text}</Text>
                  </View>
                })}
     </ParallaxScrollView>
